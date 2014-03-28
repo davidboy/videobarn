6489dfa9b37cd58b2850385378654fb8c617eb67
David Reed <dboyreed@charter.net>
2014-03-28 13:36:39 -0500

    Redirect to latest show after signin.
    The old way was to go to the user's profile, which is rather *empty* atm.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_back_or_to user
+      redirect_back_or_to Show.first
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
